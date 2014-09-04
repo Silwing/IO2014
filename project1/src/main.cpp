b3fbec59706ea98759ab8f08b05d61ddc595464e
Andreas Fyrstelin Kristiansen <AFKpost@gmail.com>
2014-09-04 14:19:58 +0200

    test added to main. TODO: move to test file?

--- project1/src/main.cpp
+++ project1/src/main.cpp
@@ -6,18 +6,31 @@
 #define OS_WIN
 #endif
 
+void test(AbstractInputStream<int>* in, AbstractOutputStream<int>* out, string testName) {
+    int zize = 100;
+    out->create();
+	for (int i = 0; i < zize; i++) {
+        out->write(i);
+    }
+    out->close();
+
+	in->open();
+    for (int i = 0; i < zize; i++) {
+        if (i != in->readNext()) {
+            printf("%s failed at element %d\n", testName.c_str(), i);
+            return;
+        }
+    }
+    if (!in->endOfStream()) {
+        printf("%s failed at endOfFile\n", testName.c_str());
+    } else {
+        printf("%s succeed\n", testName.c_str());
+    }
+}
+
 int main(int argc, char** argv) {
-
 	SingleItemOutputStream<int> out("data/foo");
-	out.create();
-	for (int i = 0; i < 10; i++) {
-		out.write(i);
-	}
-	out.close();
-
 	SingleItemInputStream<int> in("data/foo");
-	in.open();
-	while(!in.endOfStream()) {
-		printf("%d\n", in.readNext());
-	}
+
+    test(&in, &out, "SingleItems");
 }
